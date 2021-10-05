1aa1f69d7e7cb1b76a7552e1324b3e1bcfea92b8
Noname Kamisawa <noname@kamisawa.net>
2021-10-05 23:49:04 +0900

    toc追加

--- convert.sh
+++ convert.sh
@@ -4,5 +4,5 @@ SOURCE_PATH="./main.md"
 REFERENCE_PATH="./reference.docx"
 OUTPUT_PATH="/F/Dropbox/Comiket/Comic1_19/techniques-to-escape-from-dead-end-lines.docx"
 
-pandoc $SOURCE_PATH --reference-doc=$REFERENCE_PATH -o $OUTPUT_PATH
+pandoc $SOURCE_PATH --reference-doc=$REFERENCE_PATH --table-of-contents -o $OUTPUT_PATH
 
