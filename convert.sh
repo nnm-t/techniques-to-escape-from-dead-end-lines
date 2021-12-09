137df73822973ec8d4d02d58934e5c24612856a0
Noname Kamisawa <noname@kamisawa.net>
2021-12-10 00:23:11 +0900

    modify convert script

--- convert.sh
+++ convert.sh
@@ -2,7 +2,7 @@
 
 SOURCE_PATH="./main.md"
 REFERENCE_PATH="./reference.docx"
-OUTPUT_PATH="/F/Dropbox/Comiket/Comic1_19/techniques-to-escape-from-dead-end-lines.docx"
+OUTPUT_PATH="/F/Dropbox/Comiket/C99/escape-from-dead-end-lines/techniques-to-escape-from-dead-end-lines.docx"
 
 pandoc $SOURCE_PATH --reference-doc=$REFERENCE_PATH --table-of-contents -o $OUTPUT_PATH
 
